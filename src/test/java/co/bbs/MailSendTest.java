7d7a3776f4af0b1c8016b92b1373519fe33ee20d
cheng_zx <cheng_zx@suixingpay.com>
2018-08-03 09:28:08 +0800

    注释掉测试类

--- src/test/java/co/bbs/MailSendTest.java
+++ src/test/java/co/bbs/MailSendTest.java
@@ -8,39 +8,30 @@
  */
 package co.bbs;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.test.context.junit4.SpringRunner;
-
 /**
  * @description: 邮件发送测试
  * @author: 程志祥<cheng_zx@suixingpay.com>
  * @date: 2018/3/13 下午1:41
  * @version: V1.0
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = Application.class)
 public class MailSendTest {
 
-    @Autowired
-    private JavaMailSender javaMailSender;
-
-    @Value("${spring.mail.username}")
-    private String username;
-
-    @Test
-    public void testSendSimple() {
-        /*SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(username);
-        message.setTo("cheng_zx@suixingpay.com");
-        message.setSubject("标题：测试标题");
-        message.setText("测试内容部份");
-        javaMailSender.send(message);*/
-    }
+//    @Autowired
+//    private JavaMailSender javaMailSender;
+//
+//    @Value("${spring.mail.username}")
+//    private String username;
+//
+//    @Test
+//    public void testSendSimple() {
+//        /*SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom(username);
+//        message.setTo("cheng_zx@suixingpay.com");
+//        message.setSubject("标题：测试标题");
+//        message.setText("测试内容部份");
+//        javaMailSender.send(message);*/
+//    }
 
 }
